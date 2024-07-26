ea8c7e79ad2d0a5fb3b09fd73ab782e88192866f
Ashher <ashher868@gmail.com>
2024-07-26 16:33:55 +0530

    completed

---
+++ prisma/migrations/20240726104645_donate_text/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "donateText_de" TEXT DEFAULT 'Bereitwillig zu geben bedeutet mehr als zu geben',
+ADD COLUMN     "donateText_eng" TEXT DEFAULT 'Do not withhold good from those who deserve it, when it is in your power to act',
+ADD COLUMN     "donateText_ru" TEXT DEFAULT 'Давать охотно - значит больше чем много давать',
+ADD COLUMN     "donateText_ua" TEXT DEFAULT 'НДавати охоче - значить більше ніж давати';
